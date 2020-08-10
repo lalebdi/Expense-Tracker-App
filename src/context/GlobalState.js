0a7f2965ec70a2f6ce30b6c094776d43db389ae2
lalebdi <loai.alebdi@gmail.com>
2020-08-10 10:52:41 -0400

    created context

---
+++ src/context/GlobalState.js
@@ -0,0 +1,30 @@
+import React, { createContext, useReducer } from 'react';
+
+//  The initial state
+// Each transacton has an id. if it is a negative number => expense. if a positive number => its income.
+
+const initialState = {
+    transactions: [
+            { id: 1, text: 'Flower', amount: -20 },
+            { id: 2, text: 'Salary', amount: 300 },
+            { id: 3, text: 'Book', amount: -10 },
+            { id: 4, text: 'Camera', amount: 150 }
+        ]
+}
+
+// Creating context
+
+export const GlobalContext = createContext(initialState);
+
+// in order for others can have access to our globalState we need to have a provider 
+
+// Below is the Provider component
+export const GlobalProvider =({ childern }) =>{
+    const [state, dispatch] = useReducer(AppReducer, initialState)
+
+    return(<GlobalContext.Provider vlaue={{
+        transactions:state.transactions
+    }}>
+        {childern} 
+    </GlobalContext.Provider>);
+}
