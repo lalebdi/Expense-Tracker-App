8353f3d415bd3c113f5daa640ef1413f34fb93b9
lalebdi <loai.alebdi@gmail.com>
2020-08-17 11:16:56 -0400

    debugged

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,17 +1,16 @@
-export default(state, action) => {
-    switch(action.type){
+export default (state, action) => {
+    switch(action.type) {
         case 'DELETE_TRANSACTION':
-            return{
-                ...state,
-                transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
-            }
-        
+        return {
+            ...state,
+            transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
+        }
         case 'ADD_TRANSACTION':
-            return{
-                ...state,
-                transactions: [action.payload,...state.transactions]
-            }
+        return {
+            ...state,
+            transactions: [action.payload, ...state.transactions]
+        }
         default:
-            return state;
+        return state;
     }
 }
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -6,7 +6,7 @@ import AppReducer from './AppReducer';
 
 const initialState = {
     transactions: []
-}
+    }
 
 // Creating context
 
@@ -15,29 +15,29 @@ export const GlobalContext = createContext(initialState);
 // in order for others can have access to our globalState we need to have a provider 
 
 // Below is the Provider component
-export const GlobalProvider =({ childern }) =>{
-    const [state, dispatch] = useReducer(AppReducer, initialState)
+export const GlobalProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
 
     // Actions below: (add it to the reducer)
-    function deleteTransaction(id){
+    function deleteTransaction(id) {
         dispatch({
             type: 'DELETE_TRANSACTION',
             payload: id
         });
-    }
+        }
 
-    function addTransaction(transaction){
+    function addTransaction(transaction) {
         dispatch({
             type: 'ADD_TRANSACTION',
             payload: transaction
         });
-    }
+        }
 
-    return(<GlobalContext.Provider vlaue={{
-        transactions:state.transactions,
+    return (<GlobalContext.Provider value={{
+        transactions: state.transactions,
         deleteTransaction, // to use this action we have to pass it down to the provider
         addTransaction
-        }}>
-        {childern} 
-    </GlobalContext.Provider>);
+    }}>
+    {children}
+            </GlobalContext.Provider>);
 }
